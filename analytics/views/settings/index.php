498be24f33e451382a272cac0f60024a503de2e8
Shawn Crigger <support@s-vizion.com>
2012-03-16 15:28:11 -0400

    Updating stuff to work on .6-dev

--- analytics/views/settings/index.php
+++ analytics/views/settings/index.php
@@ -15,7 +15,7 @@
 		<div class="control-group <?php echo form_has_error('ga_username') ? 'error' : ''; ?>">
 			<label class="control-label" for="ga_username">Analytics username<span class="required">*</span></label>
 			<div class='controls'>
-				<input id="ga_username" type="text" name="ga_username" maxlength="100" value="<?php echo isset($settings['ga_username']) ? $settings['ga_username'] : set_value('ga_username' , ''); ?>" />
+				<input id="ga_username" type="text" name="ga_username" maxlength="100" value="<?php echo set_value('ga_username', isset($settings['ga_username']) ? $settings['ga_username'] : ''); ?>">
 				<span class="help-inline"><?php echo form_error('ga_username'); ?></span>
 			</div>
 		</div>
@@ -23,7 +23,7 @@
 		<div class="control-group <?php echo form_has_error('ga_new_password') ? 'error' : ''; ?>">
 			<label class="control-label" for="ga_new_password">Analytics password<span class="required">*</span></label>
 			<div class='controls'>
-				<input id="ga_password" type="hidden" name="ga_password" value="<?php echo isset($settings['ga_password']) ? $settings['ga_password'] : set_value('ga_password' , ''); ?>" />
+				<input id="ga_password" type="hidden" name="ga_password" value="<?php echo set_value('ga_password', isset($settings['ga_password']) ? $settings['ga_password'] : ''); ?>" />
 				<input id="ga_new_password" type="password" name="ga_new_password" maxlength="100" >
 				<span class="help-inline"><?php echo form_error('ga_new_password'); ?></span>
 			</div>
@@ -32,7 +32,7 @@
 				<div class="control-group <?php echo form_has_error('ga_code') ? 'error' : ''; ?>">
 			<label class="control-label" for="ga_code">Analytics code<span class="required">*</span></label>
 			<div class='controls'>
-				<input id="ga_code" type="text" name="ga_code" maxlength="100" value="<?php echo isset($settings['ga_code']) ? $settings['ga_code'] : set_value('ga_code' , ''); ?>" />
+				<input id="ga_code" type="text" name="ga_code" maxlength="100" value="<?php echo set_value('ga_code', isset($settings['ga_code']) ? $settings['ga_code'] : ''); ?>">
 				<span class="help-inline"><?php echo form_error('ga_code'); ?></span>
 			</div>
 		</div>
@@ -40,7 +40,7 @@
 		<div class="control-group <?php echo form_has_error('ga_enabled') ? 'error' : ''; ?>">
 			<label class="control-label" for="ga_enabled">Enabled<span class="required">*</span></label>
 			<div class='controls'>
-				<?php echo form_dropdown('ga_enabled', array('0'=>'No','1'=>'Yes'), $settings['ga_enabled'], 'id="ga_enabled" class="span2"'); ?>
+				<?php echo form_dropdown('ga_enabled', array('0'=>'No','1'=>'Yes'), set_value('ga_enabled', ( ( !empty($settings['ga_enabled']) ) ? $settings['ga_enabled'] : 0 ) ), 'id="ga_enabled" class="span2"'); ?>
 				<span class="help-inline"><?php echo form_error('ga_enabled'); ?></span>
 			</div>
 		</div>
