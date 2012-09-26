e61cd5fe97ec2eadee3f7645a6c7232cd5d7aed1
Shawn Crigger <support@s-vizion.com>
2012-09-26 00:50:24 -0400

    fix errors in views

--- analytics/views/reports/reportsjs.php
+++ analytics/views/reports/reportsjs.php
@@ -1,13 +1,9 @@
-<?php //		 head.ready(function(){
-?>
+var so = new SWFObject("<?php echo base_url()?>bonfire/modules/analytics/assets/js/amcharts/amline.swf", "amline", "100%", "250", "8", "#FFFFFF");
+so.addVariable("data_file", encodeURIComponent("<?php echo base_url()?>bonfire/modules/analytics/assets/js/amcharts/amline_data.xml"));
+so.addVariable("chart_id", "amline");
+so.addParam("wmode", "opaque");
+so.addVariable('wmode', 'opaque');
+so.addVariable("settings_file", encodeURIComponent("<?php  echo base_url()?>bonfire/modules/analytics/assets/js/amcharts/amline_settings.xml"));
+so.addVariable("path", '<?php echo base_url()?>');
+so.write('linechart');
 
-
-			var so = new SWFObject("<?php echo base_url()?>bonfire/modules/analytics/assets/js/amcharts/amline.swf", "amline", "100%", "250", "8", "#FFFFFF");
-			so.addVariable("data_file", encodeURIComponent("<?php echo base_url()?>bonfire/modules/analytics/assets/js/amcharts/amline_data.xml"));
-			so.addVariable("chart_id", "amline");
-			so.addParam("wmode", "opaque");
-			so.addVariable('wmode', 'opaque');
-			so.addVariable("settings_file", encodeURIComponent("<?php  echo base_url()?>bonfire/modules/analytics/assets/js/amcharts/amline_settings.xml"));
-			so.addVariable("path", '<?php echo base_url()?>');
-			so.write('linechart');
-  
